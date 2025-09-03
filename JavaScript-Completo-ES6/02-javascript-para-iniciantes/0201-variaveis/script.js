d6b48e206ea9c8a13169b552fa6753f6aa49449b
Rafael Pereira <rafaelairespereira07@gmail.com>
2025-09-03 18:44:52 -0300

    JavaScript - Módulo 2 Completo

---
+++ JavaScript-Completo-ES6/02-javascript-para-iniciantes/0201-variaveis/script.js
@@ -0,0 +1,21 @@
+// Declarar uma variável com o seu nome
+var nome = 'Rafael';
+
+// Declarar uma variável com a sua idade
+var idade = 28;
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+comidaFavorita = 'Pizza';
+
+// Declarar 5 variáveis diferentes sem valores
+var time,
+  professor,
+  pais,
+  endereco,
+  rua;
+
+console.log(nome, idade, comidaFavorita);
